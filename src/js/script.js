fccbcd98dd67a9df1952200aa09faf9d6e58904f
Matthieu Toussaint <matthieu.toussaint@hetic.net>
2018-10-02 22:25:08 +0200

    animation of demo :zap:

---
+++ src/js/script.js
@@ -0,0 +1,69 @@
+/***********************
+* ANIMATION OF SCREENS *
+***********************/
+
+(function () {
+
+    function toArray(arrayLike) {
+        return Array.prototype.slice.call( arrayLike );
+    }
+
+    function qs(selector) {
+        return document.querySelector( selector );
+    }
+
+    /* Main */
+
+    // var screens;
+
+    function start() {
+        var screenTimeline = new TimelineLite();
+        var width = window.innerWidth;
+
+        // Screen 1
+        screenTimeline.to( qs('.screen-01'), 0, {opacity: 1} );
+        screenTimeline.from( qs('.screen-01'), 1, {opacity: 0, y: '25%'});
+        screenTimeline.to( qs('.screen-01'), 0.5, {opacity: 0, delay: 2.5});
+
+        //Screen 2
+        screenTimeline.to( qs('.screen-02'), 1, {opacity: 1});
+        screenTimeline.from( qs('.screen-02 .tap-marker'), 0.35, {opacity: 0, delay: 0.5});
+        screenTimeline.to( qs('.screen-02 .tap-marker'), 0.25, {scale: 0.7, delay: 0.5});
+        screenTimeline.to( qs('.screen-02 .tap-marker'), 0.35, {opacity: 0, scale: 1}, '+=0.5');
+        screenTimeline.to( qs('.screen-02'), 0.5, {opacity: 0, delay: 1.5});
+
+        //Screen 3
+        screenTimeline.to( qs('.screen-03'), 0, {opacity: 1});
+        screenTimeline.from( qs('.screen-03 .tap-marker'), 0.35, {opacity: 0, delay: 0.5});
+        screenTimeline.to( qs('.screen-03 .tap-marker'), 0.25, {scale: 0.7, delay: 0.5});
+        screenTimeline.to( qs('.screen-03 .tap-marker'), 0.35, {opacity: 0, scale: 1}, '+=0.5');
+        screenTimeline.to( qs('.screen-03'), 1, {delay: 0.5});
+
+        //Screen 4
+        screenTimeline.to( qs('.screen-04'), 0, {opacity: 1, x: '+250px'}, "-=3");
+        screenTimeline.to( qs('.screen-04'), 1, {x: '0'}, '-=1.5');
+        screenTimeline.from( qs('.screen-04 .tap-marker'), 0.35, {opacity: 0, delay: 4});
+        screenTimeline.to( qs('.screen-04 .tap-marker'), 0.25, {scale: 0.7, delay: 0.5});
+        screenTimeline.to( qs('.screen-04 .tap-marker'), 0.35, {opacity: 0, scale: 1}, '+=0.5');
+        screenTimeline.to( qs('.screen-04'), 1, {delay: 0.5});
+
+        //Screen 5
+        screenTimeline.to( qs('.screen-05'), 0, {opacity: 1, x: '+250px'}, "-=3");
+        screenTimeline.to( qs('.screen-05'), 1, {x: '0'}, '-=1.5');
+        screenTimeline.from( qs('.screen-05 .tap-marker'), 0.35, {opacity: 0, delay: 2});
+        screenTimeline.to( qs('.screen-05 .tap-marker'), 0.25, {scale: 0.7, delay: 0.5});
+        screenTimeline.to( qs('.screen-05 .tap-marker'), 0.35, {opacity: 0, scale: 1}, '+=0.5');
+        screenTimeline.to( qs('.screen-05'), 0.5, {opacity: 0, delay: 1.5});
+
+        //Return to screen 4
+        screenTimeline.to( qs('.screen-04'), 0, {opacity: 1, x: '-250px'}, "-=3");
+        screenTimeline.to( qs('.screen-04'), 1, {x: '0'}, '-=1.5');
+    }
+
+    window.addEventListener('load', start);
+
+}());
+
+
+
+
