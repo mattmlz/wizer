1eb4517ec11e733757fa05b7964d0b26e9820b11
Matthieu Toussaint <matthieu.toussaint@hetic.net>
2018-10-03 15:50:39 +0200

    style modification + add a screen :art: :heavy_plus_sign:

--- src/js/script.js
+++ src/js/script.js
@@ -20,7 +20,7 @@
         var screenTimeline = new TimelineLite();
         var width = window.innerWidth;
 
-        // Screen 1
+/*        // Screen 1
         screenTimeline.to( qs('.screen-01'), 0, {opacity: 1} );
         screenTimeline.from( qs('.screen-01'), 1, {opacity: 0, y: '25%'});
         screenTimeline.to( qs('.screen-01'), 0.5, {opacity: 0, delay: 2.5});
@@ -30,18 +30,36 @@
         screenTimeline.from( qs('.screen-02 .tap-marker'), 0.35, {opacity: 0, delay: 0.5});
         screenTimeline.to( qs('.screen-02 .tap-marker'), 0.25, {scale: 0.7, delay: 0.5});
         screenTimeline.to( qs('.screen-02 .tap-marker'), 0.35, {opacity: 0, scale: 1}, '+=0.5');
-        screenTimeline.to( qs('.screen-02'), 0.5, {opacity: 0, delay: 1.5});
+        screenTimeline.to( qs('.screen-02'), 0.5, {opacity: 0, delay: 1.5});*/
 
         //Screen 3
         screenTimeline.to( qs('.screen-03'), 0, {opacity: 1});
-        screenTimeline.from( qs('.screen-03 .tap-marker'), 0.35, {opacity: 0, delay: 0.5});
-        screenTimeline.to( qs('.screen-03 .tap-marker'), 0.25, {scale: 0.7, delay: 0.5});
+
+        //first click
+        screenTimeline.from (qs('.screen-03 .tap-marker'), 0, {opacity: 0, delay: 1.5, top:'-432px', left:'200px'});
+        screenTimeline.to (qs('.screen-03 .tap-marker'), 0, {top:'-432px', left:'200px'});
+        screenTimeline.to( qs('.screen-03 .tap-marker'), 0.25, {opacity: 0.8, scale: 0.7, delay: 0.5});
         screenTimeline.to( qs('.screen-03 .tap-marker'), 0.35, {opacity: 0, scale: 1}, '+=0.5');
-        screenTimeline.to( qs('.screen-03'), 1, {delay: 0.5});
+
+        //Menu appears
+        screenTimeline.to( qs('.screen-03-menu'), 0, {opacity: 1, x: '+250px'}, "-=3");
+        screenTimeline.to( qs('.screen-03-menu'), 1, {x: '+55px'});
+
+        //second click - menu dissapears
+        screenTimeline.from (qs('.screen-03-menu .tap-marker'), 0, {opacity: 0, delay: 1.5});
+        screenTimeline.to( qs('.screen-03-menu .tap-marker'), 0.25, {opacity: 0.8, scale: 0.7, delay: 0.5});
+        screenTimeline.to( qs('.screen-03-menu .tap-marker'), 0.35, {opacity: 0, scale: 1}, '+=0.5');
+        screenTimeline.to( qs('.screen-03-menu'), 1, {x: '+250px'});
+
+        //third click - opening article
+        screenTimeline.from (qs('.screen-03 .tap-marker'), 0, {opacity: 0, delay: 1.5, top:'-245px', left:'60px'});
+        screenTimeline.to (qs('.screen-03 .tap-marker'), 0, {top:'-245px', left:'60px'});
+        screenTimeline.to( qs('.screen-03 .tap-marker'), 0.25, {opacity: 0.8, scale: 0.7, delay: 0.5});
+        screenTimeline.to( qs('.screen-03 .tap-marker'), 0.35, {opacity: 0, scale: 1}, '+=0.5');
 
         //Screen 4
         screenTimeline.to( qs('.screen-04'), 0, {opacity: 1, x: '+250px'}, "-=3");
-        screenTimeline.to( qs('.screen-04'), 1, {x: '0'}, '-=1.5');
+        screenTimeline.to( qs('.screen-04'), 1, {x: '0', delay: 2.5}, '-=1.5');
         screenTimeline.from( qs('.screen-04 .tap-marker'), 0.35, {opacity: 0, delay: 4});
         screenTimeline.to( qs('.screen-04 .tap-marker'), 0.25, {scale: 0.7, delay: 0.5});
         screenTimeline.to( qs('.screen-04 .tap-marker'), 0.35, {opacity: 0, scale: 1}, '+=0.5');
