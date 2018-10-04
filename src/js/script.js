2ae698c348ac12e6de2557d18011e8e05f075e88
Matthieu Toussaint <matthieu.toussaint@hetic.net>
2018-10-04 11:00:31 +0200

    add screen 07 :heavy_plus_sign:

--- src/js/script.js
+++ src/js/script.js
@@ -21,7 +21,7 @@
         var screenTimeline = new TimelineLite();
         var width = window.innerWidth;
 
-/*        // Screen 1
+        // Screen 1
         screenTimeline.to( qs('.screen-01'), 0, {opacity: 1} );
         screenTimeline.from( qs('.screen-01'), 1, {opacity: 0, y: '25%'});
         screenTimeline.to( qs('.screen-01'), 0.5, {opacity: 0, delay: 2.5});
@@ -34,6 +34,7 @@
         screenTimeline.to( qs('.screen-02'), 0.5, {opacity: 0, delay: 1.5});
 
         //Screen 3
+        screenTimeline.to( qs('.screen-02'), 0, {opacity: 0, delay: 0.5});
         screenTimeline.to( qs('.screen-03'), 0, {opacity: 1});
 
         //first click
@@ -50,7 +51,7 @@
         screenTimeline.from (qs('.screen-03-menu .tap-marker'), 0, {opacity: 0, delay: 1.5});
         screenTimeline.to( qs('.screen-03-menu .tap-marker'), 0.25, {opacity: 0.8, scale: 0.7, delay: 0.5});
         screenTimeline.to( qs('.screen-03-menu .tap-marker'), 0.35, {opacity: 0, scale: 1}, '+=0.5');
-        screenTimeline.to( qs('.screen-03-menu'), 1, {x: '+250px'});*/
+        screenTimeline.to( qs('.screen-03-menu'), 1, {x: '+250px'});
 
         //third click - opening article
         screenTimeline.from (qs('.screen-03 .tap-marker'), 0, {opacity: 0, delay: 1.5, top:'-245px', left:'60px'});
@@ -59,6 +60,7 @@
         screenTimeline.to( qs('.screen-03 .tap-marker'), 0.35, {opacity: 0, scale: 1}, '+=0.5');
 
         //Screen 4
+        screenTimeline.to( qs('.screen-03-menu'), 0, {opacity: 0, delay: 0.5});
         screenTimeline.to( qs('.screen-04'), 0, {opacity: 1, x: '+250px'}, "-=3");
         screenTimeline.to( qs('.screen-04'), 1, {x: '0', delay: 2.5}, '-=1.5');
         screenTimeline.to (qs('.screen-04 .tap-marker'), 0, {bottom:'400px', left:'180px'});
@@ -76,19 +78,40 @@
         screenTimeline.to( qs('.screen-05'), 0.5, {opacity: 0, delay: 1.5});
 
         //Return to screen 4
-        screenTimeline.to( qs('.screen-05'), 1, {opacity: 0, x: '+250px', delay: 0.5});
         screenTimeline.to( qs('.screen-04'), 0, {opacity: 1, x: '-250px'}, "-=3");
         screenTimeline.to( qs('.screen-04'), 1, {x: '0'}, '-=1.5');
 
         //Click to open search bar
+        screenTimeline.to( qs('.screen-03'), 0, {opacity: 0});
         screenTimeline.from (qs('.screen-04 .tap-marker'), 0, {opacity: 0, delay: 1.5, bottom:'40px', left:'100px'});
         screenTimeline.to (qs('.screen-04 .tap-marker'), 0, {bottom:'40px', left:'100px'});
         screenTimeline.to( qs('.screen-04 .tap-marker'), 0.25, {opacity: 0.8, scale: 0.7, delay: 0.5});
         screenTimeline.to( qs('.screen-04 .tap-marker'), 0.35, {opacity: 0, scale: 1}, '+=0.5');
 
         //Screen 6
+        screenTimeline.to( qs('.screen-04'), 0, {opacity: 0, delay: 0.5});
         screenTimeline.from( qs('.screen-06'), 0, {opacity: 0, y: '25%'});
         screenTimeline.to( qs('.screen-06'), 1, {opacity: 1} );
+
+        //click to open account notes
+        screenTimeline.from (qs('.screen-06 .tap-marker'), 0, {opacity: 0, delay: 1.5});
+        screenTimeline.to( qs('.screen-06 .tap-marker'), 0.25, {opacity: 0.8, scale: 0.7, delay: 0.5});
+        screenTimeline.to( qs('.screen-06 .tap-marker'), 0.35, {opacity: 0, scale: 1}, '+=0.5');
+
+        //Screen 7
+        screenTimeline.to( qs('.screen-06'), 0.5, {opacity: 0, delay: 1.5});
+        screenTimeline.to( qs('.screen-07'), 0.5, {opacity: 1});
+        screenTimeline.to( qs('.screen-03'), 0, {opacity: 1, delay: 0.5});
+
+        //back to feed
+        screenTimeline.from (qs('.screen-07 .tap-marker'), 0, {opacity: 0, delay: 1.5});
+        screenTimeline.to( qs('.screen-07 .tap-marker'), 0.25, {opacity: 0.8, scale: 0.7, delay: 0.5});
+        screenTimeline.to( qs('.screen-07 .tap-marker'), 0.35, {opacity: 0, scale: 1}, '+=0.5');
+
+        //Screen 7
+        screenTimeline.to( qs('.screen-03 .tap-marker'), 0, {opacity: 0});
+        screenTimeline.to( qs('.screen-07'), 0.5, {opacity: 0});
+        screenTimeline.to( qs('.screen-03'), 0.5, {opacity: 1});
     }
 
     window.addEventListener('load', start);
